0a69549ecc868f216ad925cebe31564b6e03fe12
angoglez <mentidero1314>
2020-01-23 12:09:15 +0100

    Initial commit. Create multi-module project.

---
+++ build.sbt
@@ -0,0 +1,77 @@
+name := "mu-Hello"
+
+version := "0.1"
+
+import ProjectPlugin._
+
+
+//////////////////////////
+////  Server Modules  ////
+//////////////////////////
+
+
+
+lazy val server_common = project in file("server/modules/common")
+lazy val server_protocol = project in file("server/modules/protocol") settings serverProtocolSettings
+lazy val server_process = project in file("server/modules/process") settings serverProcessSettings dependsOn (server_common, server_protocol)
+lazy val server_app = project in file("server/modules/app") settings serverAppSettings dependsOn server_process
+
+
+
+//////////////////////////
+////      Server      ////
+//////////////////////////
+
+lazy val allServerModules: Seq[ProjectReference] = Seq(
+  server_common,
+  server_protocol,
+  server_process,
+  server_app
+)
+
+lazy val allServerModulesDeps: Seq[ClasspathDependency] =
+  allServerModules.map(ClasspathDependency(_, None))
+
+lazy val server = project in file("server") aggregate (allServerModules: _*) dependsOn (allServerModulesDeps: _*)
+//addCommandAlias
+
+
+//////////////////////////
+////  Client Modules  ////
+//////////////////////////
+
+
+lazy val client_common = project in file("client/modules/common")
+lazy val client_process = project in file("client/modules/process") settings clientProcessSettings dependsOn (client_common, server_protocol)
+lazy val client_app = project in file("client/modules/app") settings clientAppSettings dependsOn client_process
+
+
+//////////////////////////
+////      Client      ////
+//////////////////////////
+
+lazy val allClientModules: Seq[ProjectReference] = Seq(
+  client_common,
+  client_process,
+  client_app
+)
+
+lazy val allClientModulesDeps: Seq[ClasspathDependency] =
+  allClientModules.map(ClasspathDependency(_, None))
+
+lazy val client = project in file("client") aggregate (allClientModules: _*) dependsOn (allClientModulesDeps: _*)
+//addCommandAlias
+
+//////////////////////////
+////       Root       ////
+//////////////////////////
+
+lazy val allRootModules: Seq[ProjectReference] = Seq(
+  client,
+  server
+)
+
+lazy val allRootModulesDeps: Seq[ClasspathDependency] =
+  allRootModules.map(ClasspathDependency(_, None))
+
+lazy val root = project in file(".") settings (name := "mu-ToDoList") aggregate (allRootModules: _*) dependsOn (allRootModulesDeps: _*)
---
+++ project/ProjectPlugin.scala
@@ -0,0 +1,92 @@
+import higherkindness.mu.rpc.idlgen.IdlGenPlugin.autoImport._
+import sbt.Keys._
+import sbt._
+
+object ProjectPlugin extends AutoPlugin {
+
+  override def trigger: PluginTrigger = allRequirements
+
+  object autoImport {
+
+    lazy val V = new {
+      val log4cats      = "1.0.1"
+      val muRPC         = "0.19.0"
+      val macroParadise = "2.1.1"
+      val console4cats  = "0.8.0-M1"
+      val silencer      = "1.4.3"
+      val pureconfig    = "0.12.1"
+    }
+  }
+
+  import autoImport._
+
+  //////////////////////////
+  ////      Server      ////
+  //////////////////////////
+
+  private lazy val logSettings: Seq[Def.Setting[_]] = Seq(
+    libraryDependencies ++= Seq(
+      "io.chrisdavenport" %% "log4cats-slf4j" % V.log4cats
+    )
+  )
+  lazy val serverProcessSettings: Seq[Def.Setting[_]] = logSettings ++ Seq(
+    libraryDependencies += "io.higherkindness" %% "mu-rpc-channel" % "0.20.1")
+
+  lazy val serverProtocolSettings: Seq[Def.Setting[_]] = Seq(
+    idlType := "avro",
+    srcGenSerializationType := "AvroWithSchema",
+    sourceGenerators in Compile += (srcGen in Compile).taskValue,
+    libraryDependencies ++= Seq(
+      "io.higherkindness" %% "mu-rpc-channel" % V.muRPC
+    )
+  )
+
+  lazy val serverAppSettings: Seq[Def.Setting[_]] = logSettings ++ Seq(
+    libraryDependencies ++= Seq(
+      "io.higherkindness"     %% "mu-rpc-server" % V.muRPC,
+      "com.github.pureconfig" %% "pureconfig"    % V.pureconfig)
+  )
+
+  //////////////////////////
+  ////      Client      ////
+  //////////////////////////
+
+  lazy val clientCommonSettings: Seq[Def.Setting[_]] = logSettings
+
+  lazy val clientProcessSettings: Seq[Def.Setting[_]] = logSettings ++ Seq(
+    libraryDependencies ++= Seq("io.higherkindness" %% "mu-rpc-netty" % V.muRPC))
+
+  lazy val clientAppSettings: Seq[Def.Setting[_]] = logSettings
+
+  override def projectSettings: Seq[Def.Setting[_]] =
+    Seq(
+      libraryDependencies ++=
+        Seq("com.github.ghik" % "silencer-lib" % V.silencer % Provided cross CrossVersion.full,
+          "io.higherkindness" %% "mu-common" % "0.19.0"),
+      addCompilerPlugin("org.scalamacros" % "paradise"            % V.macroParadise cross CrossVersion.full),
+      addCompilerPlugin("com.github.ghik" % "silencer-plugin"     % V.silencer cross CrossVersion.full),
+      addCompilerPlugin("com.olegpy"      %% "better-monadic-for" % "0.3.1"),
+      scalaVersion := "2.12.10",
+      name := "mu-Hello",
+      version := "0.1",
+      scalacOptions := Seq(
+        "-deprecation",
+        "-encoding",
+        "UTF-8",
+        "-feature",
+        "-language:existentials",
+        "-language:higherKinds",
+        "-language:implicitConversions",
+        "-unchecked",
+        "-Xlint",
+        "-Yno-adapted-args",
+        "-Ywarn-dead-code",
+        "-Ywarn-numeric-widen",
+        "-Ywarn-value-discard",
+        "-Xfuture",
+        "-Ywarn-unused-import",
+        "-P:silencer:pathFilters=target"
+      )
+    )
+
+}
